1869aa316463d651849b375b0dfb09c240fb7f32
jinyulink <jinyulin34@gmail.com>
2022-01-14 02:38:05 +0800

    Site updated: 2022-01-14 02:38:04

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        $('[rel="icon"]').attr('href', "/img/icon.png");
         document.title = 'ヽ(●-`Д´-)ノ | JINLK BLOG';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        $('[rel="icon"]').attr('href', "/img/icon.png");
         document.title ='( • ̀ω•́ ) 歡迎回來!  '+OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
