30fabe8ec716d1e8c823082ef1d28cc8b42f6415
jinyulink <jinyulin34@gmail.com>
2022-01-13 22:48:40 +0800

    Site updated: 2022-01-13 22:48:39

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        document.title = 'ヽ(●-`Д´-)ノ | JINLK BLOG';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        document.title ='( • ̀ω•́ ) 歡迎回來!  '+OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
